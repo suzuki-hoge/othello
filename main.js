39e0303b24e4aab86019a84abaa5b5212d7c50ff
suzuki-hoge <user.ryo@gmail.com>
2023-04-23 12:51:29 +0900

    create procedures.

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function getRounds(p) {
     return directions.map(d => next(p, d));
 }
 function takeAll(b, pred) {
-    return range(0, W + 2).flatMap(x => range(0, H + 2).map(y => ({ x: x, y: y }))).filter(p => pred(b[p.y][p.x]));
+    return range(1, W + 1).flatMap(x => range(1, H + 1).map(y => ({ x: x, y: y }))).filter(p => pred(b[p.y][p.x]));
 }
 function take(b, p, d) {
     const points = [];
@@ -38,15 +38,20 @@ function take(b, p, d) {
 function next(p, d) {
     return { x: p.x + d.x, y: p.y + d.y };
 }
+function sliceOn(ts, pred) {
+    const bs = ts.map(pred);
+    const i = bs.indexOf(true);
+    return i == -1 ? [[]] : [ts.slice(0, i), ts.slice(i + 1)];
+}
 /*
  * fundamental tests
  */
-function assertEquals(exp, act, message) {
-    if (Array.isArray(exp)) {
-        exp = JSON.stringify(exp);
-    }
+function assertEquals(act, exp, message) {
     if (Array.isArray(act)) {
         act = JSON.stringify(act);
+    }
+    if (Array.isArray(exp)) {
+        exp = JSON.stringify(exp);
     }
     console.assert(exp === act, `${message}\n  exp: ${exp}\n  act: ${act}\n`);
 }
@@ -60,3 +65,122 @@ const b = createBoard();
 assertEquals(getRounds({ x: 3, y: 3 }), [{ x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }, { x: 3, y: 2 }, { x: 3, y: 4 }, { x: 4, y: 2 }, { x: 4, y: 3 }, { x: 4, y: 4 }], 'rounds: must be 8 points.');
 assertEquals(take(b, { x: 3, y: 3 }, { x: -1, y: -1 }), [{ x: 2, y: 2 }, { x: 1, y: 1 }], 'take: must contain the upper left cells.');
 assertEquals(take(b, { x: 1, y: 1 }, { x: -1, y: -1 }), [], 'take: must be empty.');
+assertEquals(sliceOn([1, 2, 3, 4, 5], n => n === 3), [[1, 2], [4, 5]], 'sliceOn: must be 2 length arrays.');
+assertEquals(sliceOn([1, 2, 3, 4, 5], n => n === 6), [[]], 'sliceOn: must be empty.');
+/*
+ * procedures
+ */
+/*
+ * 石を初期配置する
+ */
+function putInitStones(b) {
+    b[4][4] = 'W';
+    b[4][5] = 'B';
+    b[5][4] = 'B';
+    b[5][5] = 'W';
+}
+/*
+ * 自分の石を置き、裏返せる石を自分の石にする
+ */
+function put(b, p, ownStone) {
+    if (canPut(b, p, ownStone)) {
+        b[p.y][p.x] = ownStone;
+        getReversiblePoints(b, p, ownStone).forEach(p => b[p.y][p.x] = ownStone);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+/*
+ * 手番を交代するか判定する
+ * 全ての石の Point[] を作り、その周囲の Point[] を作り、そこに 1 つでも次の手番で置けるセルがあれば交代する
+ */
+function getNextPlayer(b, current) {
+    return takeAll(b, c => ['B', 'W'].includes(c))
+        .flatMap(getRounds)
+        .some(p => canPut(b, p, change(current)))
+        ? change(current) : current;
+}
+/*
+ * p に ownStone が置けるか判定する
+ * 裏返る石があれば置ける
+ */
+function canPut(b, p, ownStone) {
+    return b[p.y][p.x] !== 'N' ? false : getReversiblePoints(b, p, ownStone).length !== 0;
+}
+/*
+ * p に ownStone を置いた場合に裏返る Point[] を返す
+ * 1 方向に直線に伸びる Point[] を作り、自分の石で囲った範囲に限定し、その範囲に相手の石があり空白がなければそこは裏返る
+ */
+function getReversiblePoints(b, p, ownStone) {
+    return directions.flatMap(d => {
+        const straightPs = take(b, p, d);
+        const surroundPs = sliceOn(straightPs, p => b[p.y][p.x] === ownStone)[0];
+        return surroundPs.some(p => b[p.y][p.x] === reverse(ownStone)) && surroundPs.every(p => b[p.y][p.x] !== 'N')
+            ? surroundPs : [];
+    });
+}
+/*
+ * debug
+ */
+// setup
+const board = createBoard();
+let player = 'B';
+putInitStones(board);
+for (const r of board) {
+    console.log(r.map(c => c === 'N' ? ' ' : c).join(''));
+}
+console.log(`next player: ${player}\n`);
+// turn
+put(board, { x: 4, y: 3 }, player);
+for (const r of board) {
+    console.log(r.map(c => c === 'N' ? ' ' : c).join(''));
+}
+player = getNextPlayer(board, player);
+console.log(`next player: ${player}\n`);
+// turn
+put(board, { x: 3, y: 5 }, player);
+for (const r of board) {
+    console.log(r.map(c => c === 'N' ? ' ' : c).join(''));
+}
+player = getNextPlayer(board, player);
+console.log(`next player: ${player}\n`);
+// out
+/*
+    GGGGGGGGGG
+    G        G
+    G        G
+    G        G
+    G   WB   G
+    G   BW   G
+    G        G
+    G        G
+    G        G
+    GGGGGGGGGG
+    next player: B
+
+    GGGGGGGGGG
+    G        G
+    G        G
+    G   B    G
+    G   BB   G
+    G   BW   G
+    G        G
+    G        G
+    G        G
+    GGGGGGGGGG
+    next player: W
+
+    GGGGGGGGGG
+    G        G
+    G        G
+    G   B    G
+    G   BB   G
+    G  WWW   G
+    G        G
+    G        G
+    G        G
+    GGGGGGGGGG
+    next player: B
+ */
